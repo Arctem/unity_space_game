79cf3e38416007b410f1691d73d9a0a73303d18a
Arctem <rarctem@gmail.com>
2014-05-31 22:09:44 -0500

    Initial commit.

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.rigidbody2D.AddForce(new Vector3(Random.Range(-200.0f, 200.0f), Random.Range(-200.0f, 200.0f), Random.Range(-200.0f, 200.0f)));
+		this.rigidbody2D.AddTorque(Random.Range(-500.0f, 500.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/ShipController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+	const float ACCEL_SCALAR = 3;
+	const float ROTAT_SCALAR = 2;
+
+	float speed;
+	float heading;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotation = Input.GetAxis("Horizontal");
+		float acceleration = Input.GetAxis("Vertical");
+
+		rotation *= ROTAT_SCALAR;
+		acceleration *= ACCEL_SCALAR;
+
+		speed += acceleration;
+
+		//this.transform.Rotate (0, 0, -rotation);
+		this.rigidbody2D.AddTorque(-rotation);
+		this.rigidbody2D.AddForce (this.transform.right * acceleration);
+		//print (this.rigidbody2D.velocity);
+	}
+}
